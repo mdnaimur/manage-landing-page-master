7708188f252a02b9a49931ba0b592ca4220e8cd6
mdnaimur <mdnaimurr@gmail.com>
2024-12-22 15:28:10 +0600

    review footer

--- src/pages/Review/ReviewFotter.tsx
+++ src/pages/Review/ReviewFotter.tsx
@@ -1,17 +1,19 @@
 export const ReviewFotter = () => {
   return (
     <section className="review_footer">
-      <div className="left">
-        <p className="revire_footer_left_text">
-          Simplify how your team works today.
-        </p>
-      </div>
-      <div className="right">
-        <a href="#">
-          <button className="right_review_btn">
-            <span className="btn">Get Started</span>
-          </button>
-        </a>
+      <div className="review_footer_div">
+        <div className="left">
+          <p className="revire_footer_left_text">
+            Simplify how your team works today.
+          </p>
+        </div>
+        <div className="right">
+          <a href="#">
+            <button className="right_review_btn">
+              <span className="hoverable">Get Started</span>
+            </button>
+          </a>
+        </div>
       </div>
     </section>
   );
